7365568f0d7415ddd7e820f1b2ada0239a0f3390
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-28 16:13:07 -0500

    add new line at end of file

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -6,7 +6,7 @@ export default class Airport {
 
   get name() { return this._name; }
 
-  set name() {
+  set name(value) {
     if (typeof value !== 'string') { throw new TypeError('name must be a string'); }
     this._name = value;
   }
