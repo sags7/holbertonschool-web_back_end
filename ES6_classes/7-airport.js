3d313b1e887de81d19b3e998f921e027b7791bbc
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-28 16:09:44 -0500

    solve task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,22 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() { return this._name; }
+
+  set name() {
+    if (typeof value !== 'string') { throw new TypeError('name must be a string'); }
+    this._name = value;
+  }
+
+  get code() { return this._code; }
+
+  set code(value) {
+    if (typeof value !== 'string') { throw new TypeError('code must be a string') }
+    this._code = value;
+  }
+
+  toString() { return `[object ${this.constructor.name}]`; }
+}
