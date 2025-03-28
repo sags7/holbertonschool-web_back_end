ef5b6867c8da4479a2fd0d214e868ee67ebe901f
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-27 19:47:13 -0500

    solve task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new TypeError('cannot instantiate abstract class');
+    }
+    if (this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+}
