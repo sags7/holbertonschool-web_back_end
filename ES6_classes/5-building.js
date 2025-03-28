ed63d1b7d85c64bb71e44167cc2752e73139c027
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-03-27 20:00:24 -0500

    fix error in line 6

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof Object.getPrototypeOf(this).evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-
-    if (this.constructor === Building) {
-      throw new TypeError('cannot instantiate abstract class');
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this.sqft = sqft;
