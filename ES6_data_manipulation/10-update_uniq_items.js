b0e4f377c6e27ffa70dd545b71a5fcbc4b5ae3ad
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-01 18:57:25 -0500

    update to use map.forEach()

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
   const newMap = new Map(map);
-  map.forEach((qty, itm) => { if (qty === 1) newMap.set(itm, 100); });
+  map.forEach((qty, itm) => { if (qty === 1) map.set(itm, 100); });
   return newMap;
 }
