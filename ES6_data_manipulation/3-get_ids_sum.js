9e844b2a7879be1902e3647235e3c7e6990533d4
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-01 14:11:14 -0500

    fix lint for task 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,7 @@
 export default function getStudentsIdsSum(listOfStudents) {
   const initialValue = 0;
-  return listOfStudents.reduce((accumulator, currentValue) => accumulator + currentValue.id,
-    initialValue);
+  return listOfStudents.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.id,
+    initialValue,
+  );
 }
