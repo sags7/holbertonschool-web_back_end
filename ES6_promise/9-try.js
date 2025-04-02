fda1d3032cf6c6300f0720198dfaabb605b74969
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-02 17:52:46 -0500

    solve task 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardRail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
+  } finally { queue.push('Guardrail was processed'); }
+  return queue;
+}
