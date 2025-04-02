d982a6f475d34c06da26bd759230ecdc87356d5d
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-02 15:35:55 -0500

    put the Error prefix back in

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((response) => response.map((obj) => (
       {
         status: obj.status,
-        value: obj.status === 'fulfilled' ? obj.value : obj.reason.toString().replace('Error: ', ''),
+        value: obj.status === 'fulfilled' ? obj.value : obj.reason.toString(),
       }
     )));
 }
