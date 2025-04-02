028e1b6d8b04f7306d3736aeb61f328372556598
Sebastian Aramburo <juansebastianaramburo@gmail.com>
2025-04-02 14:52:23 -0500

    fix .then destructured mistake

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((photoResponse, userResponse) => {
+    .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
