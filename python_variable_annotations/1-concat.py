53e92818f50c5bba50c8241d9413481ec6418153
SAG <juansebastianaramburo@gmail.com>
2025-04-24 16:55:56 -0500

    solve task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that takes
+two strings and returns a concatenation of them.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
