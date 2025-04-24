88127ad64d00d5311680a68a53023615d5f8e0d4
SAG <juansebastianaramburo@gmail.com>
2025-04-24 17:07:58 -0500

    solve task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that takes a float
+and returns the string representation of it.
+"""
+
+
+def to_str(x: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        x (float): The float to convert to string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(x)
