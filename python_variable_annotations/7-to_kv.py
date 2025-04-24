9e424242ccbae577732c9c610478636d93239cae
SAG <juansebastianaramburo@gmail.com>
2025-04-24 17:37:13 -0500

    solve task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,7 +9,7 @@ and should be annotated as a float.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
   """
   Returns a tuple consisting of k and v
 
