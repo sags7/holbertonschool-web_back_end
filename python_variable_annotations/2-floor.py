f4f556b451229f2b98c5b23875a11751130f7c0a
SAG <juansebastianaramburo@gmail.com>
2025-04-24 17:05:35 -0500

    solve task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that takes a float
+and returns the floor of it.
+"""
+
+
+def floor(x: float) -> int:
+    """
+    Returns the floor of a float.
+
+    Args:
+        x (float): The float to floor.
+
+    Returns:
+        int: The floored integer.
+    """
+    return int(x)
