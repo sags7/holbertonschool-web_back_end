d50f56674c6230c37290066349605bb7ec1d7574
SAG <juansebastianaramburo@gmail.com>
2025-04-25 12:13:48 -0500

    fix pycodestyle

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ an integer max_delay and returns a asyncio.Task.
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns a asyncio.Task that waits for a random delay
