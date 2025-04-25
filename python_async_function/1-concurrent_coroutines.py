c3c37e83fb6cafd68677c7b9940ad2bcee843a02
SAG <juansebastianaramburo@gmail.com>
2025-04-25 11:57:38 -0500

    solve task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. Spawn wait_random n times
+with the specified max_delay.
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay.
+
+    Args:
+      n (int): the times to spawn wait_random
+      max_delay (int): the max delay for wait_random
+    Returns:
+      List[float]: a list of all the delays
+    """
+    delays = []
+    tasks = [wait_random(max_delay) for task in range(n)]
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
