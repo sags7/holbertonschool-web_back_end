82ce2a0fa94ae19dd13a65261f1aa228263a90ff
sag <juansebastianaramburo@gmail.com>
2025-04-29 12:41:59 -0500

    task 0 returns floats now

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -16,4 +16,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,9 +6,7 @@ that takes no arguments.
 The coroutine will collect 10 random numbers using an async comprehension
 over async_generator, then return the 10 random numbers.
 """
-import asyncio
-import random
-from typing import List, Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
