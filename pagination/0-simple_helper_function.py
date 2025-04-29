5ec2dc0715b704d3b60fd132775f03583f970f27
sag <juansebastianaramburo@gmail.com>
2025-04-29 13:57:58 -0500

    fix pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -18,7 +18,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns:
       Tuple[int, int]: A tuple containing the start index and end index.
     """
-    if page <= 0 or page_size <= 0: return (0,0)
+    if page <= 0 or page_size <= 0:
+        return (0, 0)
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
