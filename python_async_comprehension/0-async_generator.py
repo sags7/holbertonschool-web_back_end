a21d4c86c012344f747cdaa66f1c7a52b0bef672
sag <juansebastianaramburo@gmail.com>
2025-04-29 10:30:33 -0500

    add python async comprehension folder and task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/sr/bin/env python3
+"""
+This module creates an asynchronous generator that yields numbers
+from 0 to 10 with a delay of 1 second between each yield.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    An asynchronous generator that yields numbers from 0 to 10 with a delay.
+    Yields:
+        int: A random number between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
