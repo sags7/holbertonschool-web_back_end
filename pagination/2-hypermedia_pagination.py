d4576a2ce71b4d3a093ec22194325b53591e6706
SAG <juansebastianaramburo@gmail.com>
2025-05-01 12:01:22 -0500

    solve task 2

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""This module implements a get_hyper method that takes the same arguments
+(and defaults) as get_page and returns a dictionary containing
+the following key-value pairs:
+page_size: the length of the returned dataset page
+page: the current page number
+data: the dataset page (equivalent to return from previous task)
+next_page: number of the next page, None if no next page
+prev_page: number of the previous page, None if no previous page
+total_pages: the total number of pages in the dataset as an integer
+"""
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = 'Popular_Baby_Names.csv'
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as file:
+                reader = csv.reader(file)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Function that returns the correct page of the dataset
+        or an empty list if its out of range
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+        if end > len(data):
+            end = len(data)
+        return data[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Returns a dictionary with information for
+        pagination
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        
+        page_data = self.get_page(page, page_size)
+        number_of_pages = math.ceil(len(self.dataset) / page_size)
+
+        return {
+            "page_size": len(page_data),
+            "page": page,
+            "data": page_data,
+            "next_page": page + 1 if page < number_of_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": number_of_pages
+            }
