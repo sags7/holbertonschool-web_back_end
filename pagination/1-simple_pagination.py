bfe646d834c383c379037a54aeefe89b8ac0957f
SAG <juansebastianaramburo@gmail.com>
2025-05-01 11:14:26 -0500

    solve task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+This module contains 
+"""
+index_range = __import__('0-simple_helper_function').index_range
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = 'Popular_Baby_Names.csv'
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as file:
+                reader = csv.reader(file)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Function that returns the correct page of the dataset
+        or an empty list if its out of range
+        """
+        assert page > 0 and isinstance(page, int)
+        assert page_size > 0 and isinstance(page_size, int)
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+        if end > len(data):
+            end = len(data)
+
+        return data[start:end]
+
+
+            
