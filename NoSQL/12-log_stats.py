9f617a5d05dc48aacbad869b6c83fd5f1c41c3a2
sag <juansebastianaramburo@gmail.com>
2025-05-04 20:31:08 -0500

    moved MongoClient connection to main()

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,21 +5,24 @@ Python script that provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
-def count_documents(query: dict):
+def count_documents(collection, query: dict):
     """Counts documents in a MongoDB collection"""
-    client = MongoClient("mongodb://localhost:27017/")
-    collection = client.logs.nginx
     return collection.count_documents(query)
 
 
 def main():
     """Main function"""
+
+    client = MongoClient("mongodb://localhost:27017/")
+    collection = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(f"{count_documents({})} logs")
+    print(f"{count_documents(collection, {})} logs")
     print("Methods:")
     for method in methods:
-        print(f"\tmethod {method}: {count_documents({'method': method})}")
-    print(f"{count_documents({'method': 'GET', 'path': '/status'})} status check")
+        print(f"\tmethod {method}: {count_documents(collection, {'method': method})}")
+    print(
+        f"{count_documents(collection,{'method': 'GET', 'path': '/status'})} status check"
+    )
 
 
 if __name__ == "__main__":
