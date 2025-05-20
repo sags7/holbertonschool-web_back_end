dc4830dfa6c89718f4cb209cf126c22d68abcf36
sag <juansebastianaramburo@gmail.com>
2025-05-20 15:17:22 -0500

    hange stdout.log for .write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const input = data.toString();
   if (input.length > 0) {
-    process.stdout.write(`Your name is: ${input}\r`);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
