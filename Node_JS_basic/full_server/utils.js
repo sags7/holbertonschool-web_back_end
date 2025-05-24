64abd786dd69762631df78433135120a0b7aa494
sag <juansebastianaramburo@gmail.com>
2025-05-24 11:47:39 -0500

    change back to fs

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,22 +1,27 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 
-async function readDatabase(filePath) {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = {};
-
-    for (let i = 1; i < lines.length; i += 1) {
-      const [firstname, , , field] = lines[i].split(',');
-      if (!students[field]) {
-        students[field] = [];
+function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-      students[field].push(firstname);
-    }
-    return students;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+
+      const lines = data.trim().split('\n');
+      const students = {};
+
+      for (let i = 1; i < lines.length; i += 1) {
+        const [firstname, , , field] = lines[i].split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+
+      resolve(students);
+    });
+  });
 }
 
 export default readDatabase;
